2cc6af284c5f7db72add0809a9bb5dae2a8f57d3
Iron man <thomas.sporer68@gmail.com>
2024-01-06 10:40:24 +0100

    MAJ

--- base_connaissance.h
+++ base_connaissance.h
@@ -11,11 +11,6 @@ typedef struct Base_Connaissance{
 
 typedef Base_Connaissance* BC;
 
-
-BC create_empty_base();
-BC add_rule_to_tail(BC base, Regle rule);
-Regle access_to_head_base(BC base);
-
 BC create_empty_base();
 BC add_rule_to_tail(BC base, Regle rule);
 Regle access_to_head_base(BC base);
